fb2b4b909c4087521153ae4f0d655de7c928f672
Georgy Lukyanov <georgiylukjanov@gmail.com>
2019-01-30 14:26:26 +0000

    Move the applicatiuons in separate projects.

--- src/FS.hs
+++ src/FS.hs
@@ -11,19 +11,10 @@ import qualified Prelude (Monad)
 import Control.Selective hiding (dependencies)
 import Data.Either (partitionEithers)
 import Data.Functor.Const
-import Algebra.Graph
-import Algebra.Graph.Export.Dot
-import qualified Data.Set as Set
 
 -- | We amend the standard 'Monad' constraint to include 'Selective' into
 --   the hierarchy
 type Monad m = (Selective m, Prelude.Monad m)
-
--- | A type class for keys, equipped with an associated type family that
--- can be used to determine the type of value corresponding to the key.
-class Key k where
-    -- | The name of the key. Useful for avoiding heterogeneous lists of keys.
-    showKey :: k a -> String
 
 -- | The 'FS' data type is a polymorphic state-transformer metalanguage
 --   for describing the semantics of programming languages.
@@ -40,48 +31,26 @@ class Key k where
 --   for the usage of constructions like 'write key2 (read key1)' without restriction
 --   the type constructor 'f' to be an instance of 'Applicative' (to gain a way of
 --   enclosing pure values in 'f'.)
--- type FS c k a = forall f k. (c f, Key k) => (k -> f (Value k)) ->
---                                             (k -> f (Value k) -> f (Value k)) ->
---                                             Maybe (f a)
-
 -- type Read f a  = forall k. Key k => k -> f (Value k)
 type Read k f = forall a. k a -> f a
 
--- type Write f a = forall k. Key k => k -> f (Value k) -> f (Value k)
 type Write k f = forall a. k a -> f a -> f a
 
 type FS c k a = forall f. c f => Read k f -> Write k f -> f a
 
+-- | A type class for keys, equipped with an associated type family that
+-- can be used to determine the type of value corresponding to the key.
+class Key k where
+    -- | The name of the key. Useful for avoiding heterogeneous lists of keys.
+    showKey :: k a -> String
+
 -- | Calculate data dependencies of a semantic computation
 --   The computation must have only static dependencies, hence the
---   'Applicative' constraint. In case of presence of non-static dependecies
---   'Nothing' is returned.
--- dependencies :: FS Selective a -> ([String], [String])
--- dependencies task =
---     partitionEithers . getConst $
---     task trackingRead trackingWrite
---   where trackingRead  k    = Const [Left (showKey k)]
---         trackingWrite k fv = fv *> Const [Right (showKey k)]
-
+--   'Selective' constraint.
 dependencies :: Key k => FS Selective k a -> ([String], [String])
 dependencies task =
     partitionEithers . getConst $
     task trackingRead trackingWrite
-
--- trackingRead :: k a -> Const [Either (k a) b1] b2
-trackingRead k    = Const [Left (showKey k)]
-
-trackingWrite k fv = fv *> Const [Right (showKey k)]
-
--- graph :: Ord k => (k -> ([k], [k])) -> k -> Graph k
--- graph deps key = transpose $ overlays [ star k (deps k) | k <- keys Set.empty [key] ]
---   where
---     keys seen []   = Set.toList seen
---     keys seen (x:xs)
---         | x `Set.member` seen = keys seen xs
---         | otherwise           = keys (Set.insert x seen) (deps x ++ xs)
-
--- draw :: FS Selective v -> String -> String
--- draw computation = exportAsIs . graph deps
---   where
---     deps k = maybe ([], []) dependencies $ computation k
+        where
+            trackingRead k    = Const [Left (showKey k)]
+            trackingWrite k fv = fv *> Const [Right (showKey k)]
